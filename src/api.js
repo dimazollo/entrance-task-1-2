74669e78318badc4cc67a1ac7d8113f8a250dfc1
Dmitry Volovod <dvolovod@it.ru>
2018-08-06 15:23:25 +0300

    code reformat

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import { mapServerData } from './mappers'
 
 export function loadList () {
-  return fetch('/api/stations').
-    then(response => response.json()).
-    then(mapServerData)
+  return fetch('/api/stations')
+    .then(response => response.json())
+    .then(mapServerData)
 }
 
 export function loadDetails (id) {
